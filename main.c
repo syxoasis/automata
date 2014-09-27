6d1361cf65ec41f04e83eb406a6c4a571c2b1e4e
syxoasis <sunyunxiao@gmail.com>
2014-09-27 15:06:26 +0800

    main funcation
    add main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[])
+{
+	int flag = 0;
+	if(flag == 1)
+		printf("this is a test\n", );
+	else
+		printf("hello world\n");
+}
